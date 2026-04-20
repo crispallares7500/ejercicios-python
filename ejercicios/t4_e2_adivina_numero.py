c7b5d350eb52158995a4eff6912bd248fff17270
Cristina Hernandez <crispallares7500@gmail.com>
2026-04-20 19:10:38 +0200

    Create t4_e2_adivina_numero.py

---
+++ ejercicios/t4_e2_adivina_numero.py
@@ -0,0 +1,6 @@
+numero = int (input("Adivina el número entre 1 y 10: "))
+numero_ganador = 4
+if numero == numero_ganador:
+    print("¡Felicidades! Has adivinado el número ganador.")
+else:
+    print("Lo siento, no has adivinado el número ganador. Sigue intentándolo.")
