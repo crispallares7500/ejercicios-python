a9e95c7c846561a46b15c8b7bc44c6296558615e
Cristina Hernandez <crispallares7500@gmail.com>
2026-04-20 19:02:15 +0200

    Create t4_e1_ruleta.py

---
+++ ejercicios/t4_e1_ruleta.py
@@ -0,0 +1,13 @@
+color = input("Elije un color: (rojo, verde, azul, amarillo, morado) ")
+if color == "rojo":
+    print("Rojo: mensaje de pasión y energía.")
+elif color == "verde":
+    print("Verde: mensaje de esperanza y crecimiento.")
+elif color == "azul":
+    print("Azul: mensaje de calma y serenidad.")
+elif color == "amarillo":
+    print("Amarillo: mensaje de felicidad y optimismo.")
+elif color == "morado":
+    print("Morado: mensaje de sabiduría y creatividad.")
+else:
+    print("Color no válido.")
